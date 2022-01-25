865c0dda3b003bea8bef51592e7b0eba1141b5e7
mi6gun <80161464+mi6gin@users.noreply.github.com>
2022-01-25 23:34:10 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import telebot
-
+import wikipedia, re
+from soviet_union import communism
 # Создаем экземпляр бота
 
-bot = telebot.TeleBot("ебал я ваш гитхаб твари")
+bot = telebot.TeleBot("token=communism")
+
+wikipedia.set_lang("ru")
 
 # Функция, обрабатывающая команду /start
 
@@ -25,7 +28,7 @@ def helping(message):
 
     bot.send_sticker(message.chat.id, sticker)
 
-    bot.send_message(message.chat.id, 'Пока что из доступных функций я могу предложить вам лишь вызов списка(для его вызова используйте команду /list).')
+    bot.send_message(message.chat.id, 'Пока что из доступных функций я могу предложить вам лишь вызов списка(для его вызова используйте команду /list) и поиск на Википедии(для этого используйте команду /wiki).')
 
 # Функция, обрабатывающая команду /list
 @bot.message_handler(commands=["list"])
@@ -42,10 +45,50 @@ def list_files(message):
     }
     if message.text in dict:
         bot.send_message(message.chat.id, dict[message.text])
-        if message.text == '1' and '2': bot.register_next_step_handler(message, list_files)
-    elif message.text == '/start' and '/help':bot.send_message(message.chat.id, 'Простите мистер Романов, я совсем забыла вам сказать, что в режиме "списка" команды плохо работают... Отправте ещё раз пожалуйста.')
+        if message.text == '1' or '2': bot.register_next_step_handler(message, list_files)
+    elif message.text == '/wiki' or '/help' or '/start':bot.send_message(message.chat.id, 'Простите мистер Романов, я совсем забыла вам сказать, что в режиме "списка" команды плохо работают... Отправте ещё раз пожалуйста.')
     else:bot.send_message(message.chat.id, 'Мне кажется, вы что-то не то написали мистер Романов') and bot.register_next_step_handler(message, list_files)
 
+
+@bot.message_handler(commands=["wiki"])
+def wiki(message):
+    bot.send_message(
+        message.chat.id, 'Отправьте мне любое слово, и я найду его значение на Wikipedia')
+    if message.text:
+        bot.register_next_step_handler(message, handle_text)
+
+def handle_text(message):
+    bot.send_message(message.chat.id, wikiliks(message.text))
+
+# Подфункция википедии
+def wikiliks(s):
+    try:
+        ny = wikipedia.page(s)
+        # Получаем первую тысячу символов
+        wikitext=ny.content[:1000]
+        # Разделяем по точкам
+        wikimas=wikitext.split('.')
+        # Отбрасываем всЕ после последней точки
+        wikimas = wikimas[:-1]
+        # Создаем пустую переменную для текста
+        wikitext2 = ''
+        # Проходимся по строкам, где нет знаков «равно» (то есть все, кроме заголовков)
+        for x in wikimas:
+            if not('==' in x):
+                    # Если в строке осталось больше трех символов, добавляем ее к нашей переменной и возвращаем утерянные при разделении строк точки на место
+                if(len((x.strip()))>3):
+                   wikitext2=wikitext2+x+'.'
+            else:
+                break
+        # Теперь при помощи регулярных выражений убираем разметку
+        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2=re.sub('\{[^\{\}]*\}', '', wikitext2)
+        # Возвращаем текстовую строку
+        return wikitext2
+    # Обрабатываем исключение, которое мог вернуть модуль wikipedia при запросе
+    except Exception as e:
+        return 'В энциклопедии нет информации об этом'
 
 
 
