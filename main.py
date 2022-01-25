58f252c215be1111eae997940c192283ee5f2f3f
mi6gun <80161464+mi6gin@users.noreply.github.com>
2022-01-25 23:31:01 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import telebot
-import telebot, wikipedia, re
+import wikipedia, re
 
 # Создаем экземпляр бота
-
-bot = telebot.TeleBot("5092973637:AAFkWwAc8PNSc_s8B5iEED3othSNBZvPoQs")
+from soviet_union import communism
+bot = telebot.TeleBot("token=communism")
 
 wikipedia.set_lang("ru")
 
@@ -11,48 +11,54 @@ wikipedia.set_lang("ru")
 
 @bot.message_handler(commands=["start"])
 
-def start(m, res=False):
+def starting(message):
 
-    bot.send_message(m.chat.id, 'Здравствуйте мистер Романов, я ваш ассистент Нихао!')
+    photo = open('/home/mi6gun/bot/files/start.png', 'rb')
 
-    bot.send_message(m.chat.id, 'Если вам что-нибудь понадобится, то просто воспользуйтесь командой /help, и я сразу же прийду к вам на помощь!')
+    bot.send_photo(message.chat.id, photo, 'Здравствуйте мистер Романов ✌️, меня зовут Нихао, я ваш ассистент!')
+
+    bot.send_message(message.chat.id, 'Если вам что-нибудь понадобится, то просто воспользуйтесь командой /help, и я сразу же прийду к вам на помощь!')
 
 # Функция, обрабатывающая команду /help
 @bot.message_handler(commands=["help"])
 
-def help(m, res=False):
+def helping(message):
 
-    bot.send_message(m.chat.id, 'Я рада, что вы воспользовались моим советом мистер Романов! Сейчас я вам покажу, что у нас есть в меню ;)')
+    sticker = open('/home/mi6gun/bot/files/help.webp', 'rb')
 
-    bot.send_message(m.chat.id, 'Пока что из доступных функций я могу предложить вам лишь вызов списка(для его вызова используйте команду /list), поиск на википедии(для его вызова используйте команду /wiki).')
+    bot.send_sticker(message.chat.id, sticker)
+
+    bot.send_message(message.chat.id, 'Пока что из доступных функций я могу предложить вам лишь вызов списка(для его вызова используйте команду /list) и поиск на Википедии(для этого используйте команду /wiki).')
 
 # Функция, обрабатывающая команду /list
 @bot.message_handler(commands=["list"])
 
-def list(m, res=False):
+def list(message):
+    bot.send_message(
+        message.chat.id, 'Ваш список: 1)Остальгия; 2)Microsoft word for windows(10). Для того чтобы получить доступ к файлам вышлите мне их номер в списке.')
+    bot.register_next_step_handler(message, list_files)
 
-    bot.send_message(m.chat.id, 'Ваш список: 1)Остальгия; 2)Microsoft word for windows(10). Для того чтобы получить доступ к фалам вышлите мне их номер в списке.')
-    @bot.message_handler(content_types=["text"])
-    def get_text_messages(message):
-        print(message.text)
-        answ(message)
-
-    def answ(message):
-        dict = {
-            '1': 'Остальгия: берлинская стена - https://drive.google.com/drive/folders/1-kagrWnuxGG5ZAu4OW3R_WO0nISELn6m?usp=sharing',
-            '2': 'Оффис для винды - https://drive.google.com/drive/folders/127lldOpiGAYAhgRD7kff6Pv_cmn6VO39?usp=sharing',
-        }
-        if message.text in dict:
-            bot.send_message(message.chat.id, dict[message.text])
+def list_files(message):
+    dict = {
+        '1': 'Остальгия: берлинская стена - https://drive.google.com/drive/folders/1-kagrWnuxGG5ZAu4OW3R_WO0nISELn6m?usp=sharing',
+        '2': 'Оффис для винды - https://drive.google.com/drive/folders/127lldOpiGAYAhgRD7kff6Pv_cmn6VO39?usp=sharing',
+    }
+    if message.text in dict:
+        bot.send_message(message.chat.id, dict[message.text])
+        if message.text == '1' and '2': bot.register_next_step_handler(message, list_files)
+    elif message.text == '/wiki' or '/help' or '/start':bot.send_message(message.chat.id, 'Простите мистер Романов, я совсем забыла вам сказать, что в режиме "списка" команды плохо работают... Отправте ещё раз пожалуйста.')
+    else:bot.send_message(message.chat.id, 'Мне кажется, вы что-то не то написали мистер Романов') and bot.register_next_step_handler(message, list_files)
 
 
-# Функция, обрабатывающая команду /wiki
 @bot.message_handler(commands=["wiki"])
-def wiki(m, res=False):
-    bot.send_message(m.chat.id, 'Отправьте мне любое слово, и я найду его значение на Wikipedia')
-    @bot.message_handler(content_types=["text"])
-    def handle_text(message):
-        bot.send_message(message.chat.id, wikiliks(message.text))
+def wiki(message):
+    bot.send_message(
+        message.chat.id, 'Отправьте мне любое слово, и я найду его значение на Wikipedia')
+    if message.text:
+        bot.register_next_step_handler(message, handle_text)
+
+def handle_text(message):
+    bot.send_message(message.chat.id, wikiliks(message.text))
 
 # Подфункция википедии
 def wikiliks(s):
@@ -83,6 +89,7 @@ def wikiliks(s):
     # Обрабатываем исключение, которое мог вернуть модуль wikipedia при запросе
     except Exception as e:
         return 'В энциклопедии нет информации об этом'
+
 
 
 # Запускаем бота
