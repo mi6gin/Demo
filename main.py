c6ed875884cefe973be604690ef32067bbc2b39b
mi6gun <80161464+mi6gin@users.noreply.github.com>
2022-01-26 18:00:19 +0600

    Update main.py

--- main.py
+++ main.py
@@ -45,8 +45,8 @@ def list_files(message):
     }
     if message.text in dict:
         bot.send_message(message.chat.id, dict[message.text])
-        if message.text == '1' or '2': bot.register_next_step_handler(message, list_files)
-    elif message.text == '/wiki' or '/help' or '/start':bot.send_message(message.chat.id, 'Простите мистер Романов, я совсем забыла вам сказать, что в режиме "списка" команды плохо работают... Отправте ещё раз пожалуйста.')
+        if message.text in ('1' , '2'): bot.register_next_step_handler(message, list_files)
+    elif message.text in ('/wiki' , '/help' , '/start', '/list'):bot.send_message(message.chat.id, 'Простите мистер Романов, я совсем забыла вам сказать, что в режиме "списка" команды плохо работают... Отправте ещё раз пожалуйста.')
     else:bot.send_message(message.chat.id, 'Мне кажется, вы что-то не то написали мистер Романов') and bot.register_next_step_handler(message, list_files)
 
 
@@ -58,37 +58,15 @@ def wiki(message):
         bot.register_next_step_handler(message, handle_text)
 
 def handle_text(message):
-    bot.send_message(message.chat.id, wikiliks(message.text))
+    if message.text in ('/wiki' , '/help' , '/start', '/list'):bot.send_message(message.chat.id, 'Простите мистер Романов, я совсем забыла вам сказать, что в режиме "википедии" команды плохо работают... Отправте ещё раз пожалуйста.')
+    else:
+        machina=message.text.strip().lower()
+        try:
+            final_message=wikipedia.summary(machina)
+        except wikipedia.exceptions.PageError:
+            final_message="Нормально формулируйте мыcли мистер Романов!!! Я вас не понимаю!!! Я отказываюсь так работать!!!"
+        bot.send_message(message.chat.id, final_message, parse_mode='html')
 
-# Подфункция википедии
-def wikiliks(s):
-    try:
-        ny = wikipedia.page(s)
-        # Получаем первую тысячу символов
-        wikitext=ny.content[:1000]
-        # Разделяем по точкам
-        wikimas=wikitext.split('.')
-        # Отбрасываем всЕ после последней точки
-        wikimas = wikimas[:-1]
-        # Создаем пустую переменную для текста
-        wikitext2 = ''
-        # Проходимся по строкам, где нет знаков «равно» (то есть все, кроме заголовков)
-        for x in wikimas:
-            if not('==' in x):
-                    # Если в строке осталось больше трех символов, добавляем ее к нашей переменной и возвращаем утерянные при разделении строк точки на место
-                if(len((x.strip()))>3):
-                   wikitext2=wikitext2+x+'.'
-            else:
-                break
-        # Теперь при помощи регулярных выражений убираем разметку
-        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
-        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
-        wikitext2=re.sub('\{[^\{\}]*\}', '', wikitext2)
-        # Возвращаем текстовую строку
-        return wikitext2
-    # Обрабатываем исключение, которое мог вернуть модуль wikipedia при запросе
-    except Exception as e:
-        return 'В энциклопедии нет информации об этом'
 
 
 
